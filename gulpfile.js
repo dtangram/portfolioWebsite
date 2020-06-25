7fe18c3cd29a974531c1c5a5f80dee450fcef20e
dtangram <dtangram@yahoo.com>
2020-06-25 14:01:46 -0500

    Updated SASS

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,7 @@
-const gulp = require('gulp');
-const sass = require('gulp-sass');
-const plumber = require('gulp-plumber');
+const gulp = require('gulp'),
+  sass = require('gulp-sass'),
+  minifyCSS = require("gulp-minify-css"),
+  plumber = require("gulp-plumber");
 
 // SASS Styles Task
 gulp.task('stylesSCSS', () => gulp.src('sass/main.scss')
@@ -10,9 +11,18 @@ gulp.task('stylesSCSS', () => gulp.src('sass/main.scss')
   .pipe(plumber())
   .pipe(gulp.dest('css/')));
 
+  //Minify Styles Task
+  gulp.task('minify-css', function() {
+  	return gulp.src('css/main.css')
+  	.pipe(plumber())
+  	.pipe(minifyCSS())
+  	.pipe(gulp.dest('css/'))
+  });
+
 // Watch Tasks
 gulp.task('watch', () => {
-  gulp.watch('sass/base/base.scss', ['stylesSCSS']);
+  gulp.watch(['sass/paricleSysPreload.scss', 'sass/base/base.scss'], ['stylesSCSS']);
+	gulp.watch('css/main.css', ["minify-css"]);
 });
 
-gulp.task('default', ['watch', 'stylesSCSS']);
+gulp.task('default', ['watch', 'stylesSCSS', "minify-css"]);
